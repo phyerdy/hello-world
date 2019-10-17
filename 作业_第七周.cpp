1ceac211caaa615ddae433c3f9707c10667059b7
phyerdy <55117296+phyerdy@users.noreply.github.com>
2019-10-17 20:55:09 +0800

    判断两个float变量相等

--- 作业_第七周.cpp
+++ 作业_第七周.cpp
@@ -1,21 +1,15 @@
 //实现代码判断2个float变量相等
 //误差精确到时小数点后5位
 #include <stdio.h>
-
 int main()
 {
-	float m,n,h;
-	printf("The m and n is:" );
-	scanf("%f%f", &m, &n);
-	if (m<n) {
-		h = m;
-		m = n;
-		n = h;
+	float m = 1.78000;
+	float x = 1.77999, y = 2;
+	float z = m / y, z1 = x / y;
+	if (z - z1>=1e-6) {
+		z = (m + x) / y;
 	}
-	if (m - n < 1e-6)
-		printf("yes");
-	else
-		printf("no");
-	
+	printf("Z is %.5f\n", z);
+	printf("Z1 is %.5f\n", z1);
 	return 0;
 }
